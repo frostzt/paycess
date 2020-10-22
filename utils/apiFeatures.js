9fb654dbf448108fcad663da5956871af4145180
aidenfrostbite@gmail.com <aidenfrostbite@gmail.com>
2020-10-22 10:07:52 +0530

    Created Utilities classes

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -25,5 +25,31 @@ class APIFeatures {
     } else {
       this.query = this.query.sort('-createdAt');
     }
+
+    return this;
+  }
+
+  // Limiting
+  limitFields() {
+    if (this.queryString.fields) {
+      const fieldVal = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fieldVal);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  // Pagination
+  paginate() {
+    const pageNum = this.queryString.page * 1 || 1;
+    const disLimit = this.queryString.limit * 1 || 100;
+    const skip = (pageNum - 1) * disLimit;
+
+    this.query = this.query.skip(skip).limit(disLimit);
+    return this;
   }
 }
+
+module.exports = APIFeatures;
