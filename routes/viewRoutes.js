cf6372c3f120eeef5a3552137448c0aa4ce0af04
frostzt <aidenfrostbite@gmail.com>
2021-02-10 21:11:26 +0530

    Refactored entire application

--- routes/viewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const viewController = require('./../controllers/viewsController');
-const authController = require('./../controllers/authController');
-
-const router = express.Router();
-
-router.get('/', viewController.getOverview);
-
-module.exports = router;
